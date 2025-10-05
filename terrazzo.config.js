5887017e584d06cc1508e8a16e9cc99fa706213c
Pavel Laptev <pawellaptew@gmail.com>
2025-10-05 21:39:29 +0200

    update tokens structure

--- terrazzo.config.js
+++ terrazzo.config.js
@@ -1,38 +1,38 @@
-import { defineConfig } from '@terrazzo/cli';
-import * as _ from '@terrazzo/parser';
-import css from '@terrazzo/plugin-css';
+import { defineConfig } from "@terrazzo/cli";
+import * as _ from "@terrazzo/parser";
+import css from "@terrazzo/plugin-css";
 
 function pxToRem(token) {
-	if (token.$type === 'dimension') {
-		if (token.$value.value === 'px') {
-			return token.$value.slice(0, -2) / 16 + 'rem';
-		}
-	}
+  if (token.$type === "dimension") {
+    if (token.$value.value === "px") {
+      return token.$value.slice(0, -2) / 16 + "rem";
+    }
+  }
 }
 
 function clearFxPrefix(id) {
-	if (id.includes('fx.')) {
-		return id.replace('fx.', '').replace('.', '-');
-	}
+  if (id.includes("fx.")) {
+    return id.replace("fx.", "").replace(".", "-");
+  }
 }
 
 export default defineConfig({
-tokens: './design-tokens.json',
-outDir: './tokens',
-plugins: [
-css({
-filename: 'design-tokens.css',
-modeSelectors: [
-{
-mode: 'dark',
-selectors: [':root.dark']
-}
-],
-p3: false,
-transform: pxToRem,
-generateName(variableId) {
-return clearFxPrefix(variableId);
-			}
-		})
-	]
+  tokens: "./tokens/tokens.json",
+  outDir: "./tokens",
+  plugins: [
+    css({
+      filename: "tokens.css",
+      modeSelectors: [
+        {
+          mode: "dark",
+          selectors: [":root.dark"]
+        }
+      ],
+      p3: false,
+      transform: pxToRem,
+      generateName(variableId) {
+        return clearFxPrefix(variableId);
+      }
+    })
+  ]
 });
