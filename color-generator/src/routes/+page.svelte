fb8fffc2caf3134b7d47a8d7d25f46948b041114
Pavel Laptev <pawellaptew@gmail.com>
2025-12-04 16:25:41 +0100

    drop generator because it was moved to the web package

--- color-generator/src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import ColorGenerator from "$lib/ColorGenerator.svelte";
-</script>
-
-<svelte:head>
-	<title>GitButler Design Core - Color Scale Generator</title>
-</svelte:head>
-
-<ColorGenerator />
